6b5c7284f24c136ab2c5856e83987025e7f6d577
rorymercury <12345hasek@gmail.com>
2023-01-05 17:50:21 +0300

    create user model

--- todolist/settings.py
+++ todolist/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "core.User"
 
 
 # Internationalization
