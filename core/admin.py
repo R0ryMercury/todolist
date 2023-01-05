4fe3312b4db044fe1df1c769864dd1b8e95ea682
rorymercury <12345hasek@gmail.com>
2023-01-05 18:30:10 +0300

    customize admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+    exclude = ("password",)
+    readonly_fields = ("date_joined", "last_login")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    search_fields = ("emailfirst_name", "last_name", "username")
