0c0865d7389ab3586b866f8292990b916ea49e67
gmoroz <12345hasek@gmail.com>
2023-01-10 21:26:13 +0300

    add core urls

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.views.static import serve
 
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    path("", include("core.urls")),
 ]
