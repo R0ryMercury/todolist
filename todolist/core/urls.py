8c305a0f70ecab99beab239b98f5e9cb09ff9b2a
gmoroz <12345hasek@gmail.com>
2023-01-11 14:54:24 +0300

    add profile view

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from core.views import SignUpView, LoginView
+from core.views import SignUpView, LoginView, ProfileView
 
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view()),
-    path("login/", LoginView.as_view()),
+    path("signup", SignUpView.as_view()),
+    path("login", LoginView.as_view()),
+    path("profile", ProfileView.as_view()),
 ]
--- todolist/core/views.py
+++ todolist/core/views.py
@@ -1,6 +1,10 @@
-from django.contrib.auth import login
-from rest_framework.generics import CreateAPIView, GenericAPIView
-from rest_framework.permissions import AllowAny
+from django.contrib.auth import login, logout
+from rest_framework.generics import (
+    CreateAPIView,
+    GenericAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from core.models import User
 from core.serializers import CreateUserSerializer, LoginSerializer, UserSerializer
@@ -30,3 +34,16 @@ class LoginView(GenericAPIView):
         login(request, user=user)
         user_serializer = UserSerializer(instance=user)
         return Response(user_serializer.data)
+
+
+class ProfileView(RetrieveUpdateDestroyAPIView):
+    model = User
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response({})
