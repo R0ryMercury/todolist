b58f9bc9ff7e34ae9c31296dd449bfd6676774ab
gmoroz <12345hasek@gmail.com>
2023-01-12 00:01:49 +0300

    add goal cat view

--- todolist/goals/views.py
+++ todolist/goals/views.py
@@ -1,4 +1,8 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from rest_framework import permissions
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import filters
@@ -28,3 +32,17 @@ class GoalCategoryListView(ListAPIView):
 
     def get_queryset(self):
         return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
+
+
+class GoalCategoryView(RetrieveUpdateDestroyAPIView):
+    model = GoalCategory
+    serializer_class = GoalCategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
+
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.save()
+        return instance
