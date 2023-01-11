33fa0457a4153f12b39ddf947427386022567fba
gmoroz <12345hasek@gmail.com>
2023-01-11 23:47:00 +0300

    add goal create view

--- todolist/goals/views.py
+++ todolist/goals/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework import permissions
 
-# Create your views here.
+from goals.models import GoalCategory
+
+
+class GoalCategoryCreateView(CreateAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ...
