a45679e44b3e38a6a760b82b79f2048ac10b7851
gmoroz <12345hasek@gmail.com>
2023-01-11 13:25:09 +0300

    add CreateUserSerializer

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -1,6 +1,33 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from core.models import User
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField()
-    
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[])
+    password_repeat = serializers.CharField()
+
+    class Meta:
+        model = User
+        read_only_fields = ("id",)
+        fields = (
+            "id",
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+            "password_repeat",
+        )
+
+    def validate(self, attrs):
+        password = attrs.get("password")
+        password_repeat = attrs.pop("password", None)
+        if password != password_repeat:
+            raise ValidationError("passwords don't match")
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        self.user = user
+        return user
