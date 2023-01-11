f98c67e2c5fe0dba7693cb5f1c8b6448af3c3933
gmoroz <12345hasek@gmail.com>
2023-01-12 00:00:03 +0300

    add pagination and filters

--- todolist/goals/views.py
+++ todolist/goals/views.py
@@ -1,5 +1,7 @@
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework import permissions
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import filters
 
 from goals.models import GoalCategory
 from goals.serializers import GoalCategorySerializer, GoalCreateSerializer
@@ -15,6 +17,14 @@ class GoalCategoryListView(ListAPIView):
     model = GoalCategory
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = GoalCategorySerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    ordering_fields = ["title", "created"]
+    ordering = ["title"]
+    search_fields = ["title"]
 
     def get_queryset(self):
         return GoalCategory.objects.filter(user=self.request.user, is_deleted=False)
