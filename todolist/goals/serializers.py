be6e566f68b9af0bc47bcbf505b57e3b63bb2d9c
gmoroz <12345hasek@gmail.com>
2023-01-12 18:19:18 +0300

    add board create serializer

--- todolist/goals/serializers.py
+++ todolist/goals/serializers.py
@@ -1,6 +1,26 @@
 from rest_framework import serializers
-from goals.models import Goal, GoalCategory, GoalComment
+from django.db import transaction
+
+from core.models import User
 from core.serializers import UserSerializer
+from goals.models import GoalCategory, Goal, GoalComment, Board, BoardParticipant
+
+
+class BoardCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Board
+        read_only_fields = ("id", "created", "updated")
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user = validated_data.pop("user")
+        board = Board.objects.create(**validated_data)
+        BoardParticipant.objects.create(
+            user=user, board=board, role=BoardParticipant.Role.owner
+        )
+        return board
 
 
 class GoalCategorySerializer(serializers.ModelSerializer):
@@ -9,7 +29,7 @@ class GoalCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = GoalCategory
         fields = "__all__"
-        read_only_fields = ("id", "created", "updated", "user")
+        read_only_fields = ("id", "created", "updated", "user", "board")
 
 
 class GoalCategoryCreateSerializer(serializers.ModelSerializer):
@@ -33,6 +53,13 @@ class GoalCreateSerializer(serializers.ModelSerializer):
     def validate_category(self, value):
         if value.is_deleted:
             raise serializers.ValidationError("No access to the deleted category.")
+
+        if not BoardParticipant.objects.filter(
+            board_id=value.board_id,
+            role__in=[BoardParticipant.Role.owner, BoardParticipant.Role.writer],
+            user=self.context["request"].user,
+        ).exists():
+            raise serializers.ValidationError("must be owner or writer in project")
         return value
 
 
@@ -47,6 +74,9 @@ class GoalSerializer(serializers.ModelSerializer):
     def validate_category(self, value):
         if value.is_deleted:
             raise serializers.ValidationError("No access to the deleted category.")
+
+        if self.instance.category.board_id != value.board_id:
+            raise serializers.ValidationError("transfer between projects not allowed")
         return value
 
 
