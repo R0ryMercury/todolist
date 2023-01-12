0a1eab6fd2fb43e5892610036a562ebdefeeb9df
gmoroz <12345hasek@gmail.com>
2023-01-12 12:45:38 +0300

    refactor goal category model

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -18,7 +18,7 @@ class DatesModelMixin(models.Model):
         return super.save(*args, **kwargs)
 
 
-class GoalCategory(models.Model):
+class GoalCategory(DatesModelMixin):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
@@ -26,12 +26,5 @@ class GoalCategory(models.Model):
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
 
-    def save(self, *args, **kwargs):
-        if not self.id:  # Когда объект только создается, у него еще нет id
-            self.created = timezone.now()  # проставляем дату создания
-        self.updated = timezone.now()  # проставляем дату обновления
-        return super().save(*args, **kwargs)
 
