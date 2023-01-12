68e6c9c9c3132efcc07a5e0b1797815de6bf51f9
gmoroz <12345hasek@gmail.com>
2023-01-12 12:59:07 +0300

    add Goal model

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -28,3 +28,45 @@ class GoalCategory(DatesModelMixin):
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
 
 
+class Goal(DatesModelMixin):
+    class Meta:
+        verbose_name = "Цель"
+        verbose_name_plural = "Цели"
+
+    class Status(models.IntegerChoices):
+        to_do = 1, "К выполнению"
+        in_progress = 2, "В прогрессе"
+        done = 3, "Выполнено"
+        archived = 4, "Архив"
+
+    class Priority(models.IntegerChoices):
+        low = 1, "Низкий"
+        medium = 2, "Средний"
+        high = 3, "Высокий"
+        critical = 4, "Критический"
+
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        related_name="goals",
+        on_delete=models.PROTECT,
+    )
+    category = models.ForeignKey(
+        GoalCategory, verbose_name="Категория", on_delete=models.PROTECT
+    )
+    title = models.CharField(verbose_name="Заголовок", max_length=255)
+    description = models.TextField(
+        verbose_name="Описание", null=True, blank=True, default=None
+    )
+    due_date = models.DateField(
+        verbose_name="Дата выполнения", null=True, blank=True, default=None
+    )
+    status = models.PositiveSmallIntegerField(
+        verbose_name="Статус", choices=Status.choices, default=Status.to_do
+    )
+    priority = models.PositiveSmallIntegerField(
+        verbose_name="Приоритет", choices=Priority.choices, default=Priority.medium
+    )
+
+    def __str__(self) -> str:
+        return self.title
