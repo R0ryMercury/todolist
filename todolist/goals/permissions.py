04633adaceca0ccb59d599d506920a69872ec04f
gmoroz <12345hasek@gmail.com>
2023-01-12 17:57:50 +0300

    add board permissions

--- todolist/goals/permissions.py
+++ todolist/goals/permissions.py
@@ -1,30 +1,49 @@
 from rest_framework import permissions
 
+from goals.models import BoardParticipant
+
+
+class BoardPermissions(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
+        if request.method in permissions.SAFE_METHODS:
+            return BoardParticipant.objects.filter(
+                user=request.user, board=obj
+            ).exists()
+        return BoardParticipant.objects.filter(
+            user=request.user, board=obj, role=BoardParticipant.Role.owner
+        ).exists()
+
 
 class GoalCategoryPermissions(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if hasattr(obj, "user"):
-            return (
-                request.user
-                and request.user.is_authenticated
-                and obj.user == request.user
-            )
-        return (
-            request.user and request.user.is_authenticated and obj.user == request.user
-        )
+        if not request.user.is_authenticated:
+            return False
+        if request.method in permissions.SAFE_METHODS:
+            return BoardParticipant.objects.filter(
+                user=request.user, board=obj.board
+            ).exists()
+        return BoardParticipant.objects.filter(
+            user=request.user,
+            board=obj.board,
+            role__in=[BoardParticipant.Role.owner, BoardParticipant.Role.writer],
+        ).exists()
 
 
 class GoalPermissions(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if hasattr(obj, "user"):
-            return (
-                request.user
-                and request.user.is_authenticated
-                and obj.user == request.user
-            )
-        return (
-            request.user and request.user.is_authenticated and obj.user == request.user
-        )
+        if not request.user.is_authenticated:
+            return False
+        if request.method in permissions.SAFE_METHODS:
+            return BoardParticipant.objects.filter(
+                user=request.user, board=obj.category.board
+            ).exists()
+        return BoardParticipant.objects.filter(
+            user=request.user,
+            board=obj.category.board,
+            role__in=[BoardParticipant.Role.owner, BoardParticipant.Role.writer],
+        ).exists()
 
 
 class CommentPermissions(permissions.BasePermission):
@@ -33,4 +52,4 @@ class CommentPermissions(permissions.BasePermission):
             return False
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user == request.user
+        return obj.user == request.
