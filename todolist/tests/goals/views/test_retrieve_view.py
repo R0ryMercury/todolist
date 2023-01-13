d8e8e751acf7e3ed39cf09ae05fae674d539ccbb
gmoroz <12345hasek@gmail.com>
2023-01-14 01:53:36 +0300

    add  goal retrieve test

--- todolist/tests/goals/views/test_retrieve_view.py
+++ todolist/tests/goals/views/test_retrieve_view.py
@@ -5,6 +5,15 @@ from goals.serializers import GoalSerializer
 
 
 @pytest.mark.django_db
-def test_auth_required(client, board):
+def test_auth_required(client):
     response = client.get("/goals/goal/{goal.id}")
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_goal_retrieve(client, user, goal, board_participant):
+    client.force_login(user)
+    response = client.get(f"/goals/goal/{goal.id}")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == GoalSerializer(goal).data
