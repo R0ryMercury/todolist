bdbd3404dd5d4ef0ef8bbafb7e627353f7f193f6
gmoroz <12345hasek@gmail.com>
2023-01-14 01:25:39 +0300

    add success update pass test

--- todolist/tests/core/views/test_update_password.py
+++ todolist/tests/core/views/test_update_password.py
@@ -26,3 +26,25 @@ def test_invalid_old_password(client, user, faker):
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
     assert response.json() == {"old_password": ["field is incorrect"]}
+
+
+@pytest.mark.django_db
+def test_success(client, user, faker):
+    old_password = faker.password()
+    user.set_password(old_password)
+    user.save(update_fields=("password",))
+
+    new_password = faker.password()
+    client.force_login(user)
+    response = client.patch(
+        UPDATE_PASS_URL,
+        data={
+            "old_password": old_password,
+            "new_password": new_password,
+        },
+        content_type="application/json",
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert not response.json()
+    user.refresh_from_db(fields=("password",))
+    assert user.check_password(new_password)
