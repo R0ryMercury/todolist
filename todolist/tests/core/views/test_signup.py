0f9f4785f046b82592eaeed1550a999eed4d1465
gmoroz <12345hasek@gmail.com>
2023-01-14 01:05:00 +0300

    add test success short

--- todolist/tests/core/views/test_signup.py
+++ todolist/tests/core/views/test_signup.py
@@ -21,3 +21,28 @@ def test_passwords_dont_match(client, faker):
     assert response.json() == {"non_field_errors": ["passwords don't match"]}
 
 
+@pytest.mark.django_db
+def test_success_short(client, faker):
+    assert not User.objects.count()
+    password = faker.password()
+    response = client.post(
+        SIGNUP_URL,
+        data={
+            "username": faker.user_name(),
+            "password": password,
+            "password_repeat": password,
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    assert User.objects.count() == 1
+
+    user = User.objects.last()
+    assert response.json() == {
+        "id": user.id,
+        "username": user.username,
+        "first_name": "",
+        "last_name": "",
+        "email": "",
+    }
+    assert user.password != password
+    assert user.check_password(password)
