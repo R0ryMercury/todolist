26071c0427a6714d81adcaa8e37ceec915212fcb
gmoroz <12345hasek@gmail.com>
2023-01-13 17:10:46 +0300

    add tg user model

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class TgUser(models.Model):
+    tg_chat_id = models.BigIntegerField(verbose_name="tg chat id")
+    tg_id = models.BigIntegerField(verbose_name="tg id", unique=True)
+    user = models.ForeignKey(
+        "core.User",
+        models.PROTECT,
+        null=True,
+        blank=True,
+        default=None,
+        verbose_name="Пользователь в системе",
+    )
+
+    class Meta:
+        verbose_name = "tg пользователь"
+        verbose_name_plural = "tg пользователи"
