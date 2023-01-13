38f54a5e454a000ed99598c3e307a0b540a373bc
gmoroz <12345hasek@gmail.com>
2023-01-14 00:02:42 +0300

    add test post without one field

--- todolist/tests/core/test_login.py
+++ todolist/tests/core/test_login.py
@@ -1,14 +1,15 @@
 import pytest
-from django.urls import reverse
 from rest_framework import status
 
 from core.models import User
+
+LOGIN_URL = "/core/login"
 
 
 @pytest.mark.django_db
 def test_user_not_found(client, user: User):
     response = client.post(
-        "/core/login",
+        LOGIN_URL,
         data={
             "username": user.username,
             "password": user.password,
@@ -18,3 +19,13 @@ def test_user_not_found(client, user: User):
     assert response.json() == {
         "non_field_errors": ["username or password is incorrect"]
     }
+
+@pytest.mark.django_db
+def test_post_without_one_field(client, user: User):
+    response = client.post(LOGIN_URL, data={"username": user.username})
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"password": ["This field is required."]}
+
+    response = client.post(LOGIN_URL, data={"password": user.password})
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"username": ["This field is required."]}
