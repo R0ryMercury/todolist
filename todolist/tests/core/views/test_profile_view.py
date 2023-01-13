0e1da1434b8191a76e62f58b2fae566ee4930b57
gmoroz <12345hasek@gmail.com>
2023-01-14 00:45:14 +0300

    add profile success test

--- todolist/tests/core/views/test_profile_view.py
+++ todolist/tests/core/views/test_profile_view.py
@@ -12,3 +12,20 @@ def test_auth_required(client):
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
+@pytest.mark.django_db
+def test_success(client, user, faker):
+    user.first_name = faker.first_name()
+    user.last_name = faker.last_name()
+    user.save(update_fields=("first_name", "last_name"))
+
+    client.force_login(user)
+    response = client.get(PROFILE_URL)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "id": user.id,
+        "username": user.username,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email,
+    }
