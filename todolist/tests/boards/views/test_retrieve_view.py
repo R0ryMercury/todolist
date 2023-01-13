b3235bbc059bcfc039085383a063de31f8e80b15
gmoroz <12345hasek@gmail.com>
2023-01-14 01:48:25 +0300

    add retrieve board retrieve test

--- todolist/tests/boards/views/test_retrieve_view.py
+++ todolist/tests/boards/views/test_retrieve_view.py
@@ -5,6 +5,15 @@ from goals.serializers import BoardSerializer
 
 
 @pytest.mark.django_db
-def test_auth_required(client, board, user, board_participant):
+def test_auth_required(client, board):
     response = client.get(f"/goals/board/{board.pk}")
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_retrieve_board(client, user, board, board_participant):
+    client.force_login(user)
+    response = client.get(f"/goals/board/{board.pk}")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == BoardSerializer(board).data
