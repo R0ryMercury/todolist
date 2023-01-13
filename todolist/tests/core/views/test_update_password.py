61d52789ca019d9e09342b9e3b928cac45f5a678
gmoroz <12345hasek@gmail.com>
2023-01-14 01:24:05 +0300

    add invalid old password test

--- todolist/tests/core/views/test_update_password.py
+++ todolist/tests/core/views/test_update_password.py
@@ -11,3 +11,18 @@ def test_auth_required(client):
         UPDATE_PASS_URL,
     )
     assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_invalid_old_password(client, user, faker):
+    client.force_login(user)
+    response = client.patch(
+        UPDATE_PASS_URL,
+        data={
+            "old_password": faker.password(),
+            "new_password": faker.password(),
+        },
+        content_type="application/json",
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+    assert response.json() == {"old_password": ["field is incorrect"]}
