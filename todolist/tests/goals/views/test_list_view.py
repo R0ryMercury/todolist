626e10c15975a5087eaff06dddede17bc774ba0e
gmoroz <12345hasek@gmail.com>
2023-01-14 01:56:51 +0300

    add test no goals

---
+++ todolist/tests/goals/views/test_list_view.py
@@ -0,0 +1,14 @@
+import pytest
+from rest_framework import status
+
+from goals.models import BoardParticipant, Board
+
+GOAL_URL = "/goals/goal/list"
+
+
+@pytest.mark.django_db
+def test_no_goals(client, user):
+    client.force_login(user)
+    response = client.get(GOAL_URL)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
