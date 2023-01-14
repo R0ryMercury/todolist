62c357e4145f79f1d5f678c37f945df6fad5d2cd
gmoroz <12345hasek@gmail.com>
2023-01-14 13:59:23 +0300

    add cancel command

--- todolist/bot/management/commands/runbot.py
+++ todolist/bot/management/commands/runbot.py
@@ -5,7 +5,7 @@ from todolist import settings
 from bot.tg.client import TgClient
 from bot.tg.dc import Message
 from bot.models import TgUser
-from goals.models import Goal, GoalCategory
+from goals.models import BoardParticipant, Goal, GoalCategory
 
 
 class Command(BaseCommand):
@@ -38,32 +38,56 @@ class Command(BaseCommand):
 
             resp_msg = (f"#{cat.id} {cat.title}" for cat in cats)
             self.tg_client.send_message(msg.chat.id, "\n".join(resp_msg))
-            self.tg_client.is_creating = True
+
+            tg_user.is_creating = True
+            tg_user.save(update_fields=["is_creating"])
         else:
             self.tg_client.send_message(msg.chat.id, "[categories list is empty]")
 
     def choose_cat(self, msg: Message, tg_user: TgUser):
-        cat = (
-            GoalCategory.objects.fiter(user=tg_user.user).filter(title=msg.text).first()
-        )
+        cat = GoalCategory.objects.filter(user=tg_user.user, title=msg.text).first()
         if cat:
             resp_msg = f"you have selected a category {cat}"
             self.tg_client.send_message(msg.chat.id, resp_msg)
-            
+
+            tg_user.is_creating = False
+            tg_user.cat_choosen = cat.title
+            tg_user.save(update_fields=["is_creating", "cat_choosen"])
+
         else:
             resp_msg = f"The category '{msg.text}' does not exist"
+            self.fetch_cats(msg, tg_user)
+
+    def create_goal(self, msg: Message, tg_user: TgUser):
+        board_participant = BoardParticipant.objects.filter(
+            user=tg_user.user, role=BoardParticipant.Role.owner
+        )
+
+    def cancel(self, msg: Message, tg_user: TgUser):
+        tg_user.is_creating = False
+        tg_user.cat_choosen = None
+        tg_user.save(update_fields=["is_creating", "cat_choosen"])
+        self.tg_client.send_message(msg.chat.id, "[operation successfully canceled]")
 
     def handle_verified_user(self, msg: Message, tg_user: TgUser):
         if not msg.text:
             return
-        if self.tg_client.is_creating:
-            self.choose_cat(msg, tg_user)
+
         match msg.text.replace(" ", ""):
             case "/goals":
                 self.fetch_goals(msg, tg_user)
             case "/create":
                 self.fetch_cats(msg, tg_user)
+            case "/cancel":
+                self.cancel(msg, tg_user)
             case _:
+                if tg_user.is_creating:
+                    self.choose_cat(msg, tg_user)
+                    return
+                if tg_user.cat_choosen:
+                    self.create_goal(msg, tg_user)
+                    return
+
                 self.tg_client.send_message(msg.chat.id, "[unknown command]")
 
     def handle_message(self, msg: Message):
